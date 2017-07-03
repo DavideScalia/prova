f394407f329462793d3be915aec39615ee84bfab
Davide <Davide@Davide-TOSH.homenet.telecomitalia.it>
2017-07-03 17:11:21 +0200

    prova bottoni

--- src/it/polito/tdp/prova/ProvaController.java
+++ src/it/polito/tdp/prova/ProvaController.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.prova;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class ProvaController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
 }
